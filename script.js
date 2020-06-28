2e3b6ea754fc581b484c79c611011edbe7493622
Alex Rivas S <rivasalexgerman@yahoo.com.ar>
2020-06-28 12:35:36 -0300

    Internet

--- script.js
+++ script.js
@@ -36,25 +36,8 @@ open.addEventListener("click", function(){
         changeIcon = true;
     }
 });
-// Handle IE and more capable browsers
-var xhr = new ( window.ActiveXObject || XMLHttpRequest )( "Microsoft.XMLHTTP" );
-var status;
 
-// Open new request as a HEAD to the root hostname with a random param to bust the cache
-xhr.open( "HEAD", "//" + window.location.hostname + "/?rand=" + Math.floor((1 + Math.random()) * 0x10000), false );
-
-// Issue request and handle response
-try {
-xhr.send();
-  $('#gform').on('submit', function(e) {
-  $('#gform *').fadeOut(300);
-  $('#gform').prepend("<h3>Mensaje enviado!!<br><br>Gracias</h3>");
-});
-console.log ( xhr.status >= 200 && xhr.status < 300 || xhr.status === 304 );
-} catch (error) {
 $('#gform').on('submit', function(e) {
 $('#gform *').fadeOut(300);
-$('#gform').prepend("<h3>Sin internet!!<br></h3>");
+$('#gform').prepend("<h3>Mensaje enviado!<br><br>Gracias</h3>");
 });
-console.log(error);
-}
